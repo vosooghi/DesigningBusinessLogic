18a09306e8d77cfb3e034f118131ff3c91c73a49
Abbas Vosoughi <abbas769@hotmail.com>
2023-11-14 10:16:51 +0330

    Project created. init.

---
+++ TransactionScript/TransactionScript.Endpoint.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TransactionScript.Endpoint.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
